c218109b1290a46ae59554644ac86feb4ada9df8
lucascoelhosi <lucascoelhosi96@gmail.com>
2020-04-30 21:33:02 -0300

    function Filter By Gender and Tests

--- src/filter.js
+++ src/filter.js
@@ -2,6 +2,47 @@ function filterByStatus(personagens, status) {
     return personagens.filter(personagem => personagem.status === status);
 }
 
+function filterByGender(personagens, gender) {
+    return personagens.filter(personagem => personagem.gender === gender);
+}
+
+function getEpisodeFromURL(url) {
+    const urlSplited = url.split("/");
+    return urlSplited[urlSplited.length - 1];
+  }
+  
+  function generateEpisodeList(character) {
+    return character.episode.map(url => getEpisodeFromURL(url));
+  }
+  
+  function mapCharacterToEpisodes(episodes, character) {
+    const characterEpisodes = generateEpisodeList(character);
+    let newEpisodes = { ...episodes };
+    characterEpisodes.map(episode => {
+      if (newEpisodes[episode]) {
+        newEpisodes = {
+          ...newEpisodes,
+          [episode]: [...newEpisodes[episode], character]
+        };
+        return;
+      }
+  
+      newEpisodes[episode] = [character];
+    });
+    return newEpisodes;
+  }
+  
+  function generateEpisodesCharacters(characters) {
+    return characters.reduce((episodes, character) => {
+      return mapCharacterToEpisodes(episodes, character);
+    }, {});
+  }
+
 module.exports = {
-    filterByStatus
+    filterByStatus,
+    filterByGender,
+    getEpisodeFromURL,
+    generateEpisodeList,
+    mapCharacterToEpisodes,
+    generateEpisodesCharacters
 }
--- src/filter.spec.js
+++ src/filter.spec.js
@@ -20,11 +20,35 @@ describe('Filter', () => {
         })
     })
 
-    // describe('Sexo', () => {
-    //     it("Retorna somente personagens do sexo masculino", () => {
-    //         const response = filter.filterBySex(data.results, "Male");
-    //         expect(response.length).toBe(15);
-    //     })
-    // })
+    describe('Sexo', () => {
+        it("Retorna somente personagens do sexo masculino", () => {
+            const response = filter.filterByGender(data.results, "Male");
+            expect(response.length).toBe(15);
+        })
+
+        it("Retorna somente personagens do sexo feminino", () => {
+            const response = filter.filterByGender(data.results, "Female");
+            expect(response.length).toBe(4);
+        })
+
+        it("Retorna somente personagens sem sexo definido", () => {
+            const response = filter.filterByGender(data.results, "unknown");
+            expect(response.length).toBe(1);
+        })
+    })
+
+    describe('Episódios', () => {
+        
+        it("Retorna o Ep 6 da URL", () => {
+            const url = "https://rickandmortyapi.com/api/episode/6";
+            expect(filter.getEpisodeFromURL(url)).toBe('6');
+        })
+
+        // it("Retorna somente Rick e Morty para o Ep 1", () => {
+        //     const response = filter.filterByEpisode('1', data.results);
+        //     expect(response.length).toBe(2);
+        //     expect(response[1]).toBe("Morty Smith");
+        // })
+    })
 
 })
